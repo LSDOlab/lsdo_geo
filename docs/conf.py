5bfc96cac68aed5f6fe782d9a68d30510fd73395
Andrew Fletcher <afletcher168@gmail.com>
2023-06-05 13:28:18 -0700

    Update to help produce documentation

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'lsdo_project_template'
-copyright = '2023, Anugrah'
-author = 'Anugrah'
+project = 'lsdo_geo'
+copyright = '2023, Andrew Fletcher'
+author = 'Andrew Fletcher'
 version = '0.1'
 # release = 0.1.0rtc
 
@@ -50,7 +50,7 @@ nb_execution_mode = 'off'
 # autodoc_typehints = 'description'
 
 # autoapi options
-autoapi_dirs = ["../lsdo_project_template/core"]
+autoapi_dirs = ["../lsdo_geo/core"]
 autoapi_root = 'src/autoapi'
 autoapi_type = 'python'
 autoapi_file_patterns = ['*.py', '*.pyi']
