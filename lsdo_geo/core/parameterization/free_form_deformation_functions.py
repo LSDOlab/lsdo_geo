c554db2e95677400542813a2d534302e930c1531
Andrew Fletcher <afletcher168@gmail.com>
2024-02-06 11:40:20 -0800

    added ffd function for making ffd block from corners

--- lsdo_geo/core/parameterization/free_form_deformation_functions.py
+++ lsdo_geo/core/parameterization/free_form_deformation_functions.py
@@ -42,3 +42,38 @@ def construct_ffd_block_around_entities(name:str, entities:list[Union[np.ndarray
                          num_physical_dimensions=num_physical_dimensions, embedded_entities=entities)
     
     return ffd_block
+
+
+def construct_ffd_block_from_corners(name:str, entities:list[Union[np.ndarray, m3l.Variable, BSpline, BSplineSet, BSplineSubSet]],
+                                     corners:np.ndarray,
+                                        num_coefficients:tuple[int]=5, order:tuple[int]=2, num_physical_dimensions:int=3):
+    '''
+    Constructs an FFD block around the given entities and embeds them within.
+    '''
+    if type(entities) is not list:
+        entities = [entities]
+
+    from lsdo_geo.splines.b_splines.b_spline_functions import create_cartesian_enclosure_block
+    from lsdo_geo.splines.b_splines.b_spline_functions import create_b_spline_from_corners
+    # loop over entities and get points to create enclosure block
+    enclosed_points = []
+    for entity in entities:
+        if type(entity) is np.ndarray:
+            enclosed_points.append(entity)
+        elif type(entity) is m3l.Variable:
+            enclosed_points.append(entity.value)
+        elif type(entity) is BSpline or type(entity) is BSplineSet or type(entity) is Geometry:
+            enclosed_points.append(entity.coefficients.value)
+        elif type(entity) is BSplineSubSet:
+            enclosed_points.append(entity.get_coefficients().value)
+        else:
+            raise Exception("Please pass in a valid entity type.")
+    enclosed_points = np.hstack(enclosed_points)
+
+    b_spline_hyper_volume = create_b_spline_from_corners(name, corners, order=order, num_coefficients=num_coefficients, knot_vectors=None)
+    b_spline_hyper_volume.coefficients.name = entities[0].name + '_ffd_block_coefficients'
+    
+    ffd_block = FFDBlock(name=name, space=b_spline_hyper_volume.space, coefficients=b_spline_hyper_volume.coefficients,
+                         num_physical_dimensions=num_physical_dimensions, embedded_entities=entities)
+    
+    return ffd_block
